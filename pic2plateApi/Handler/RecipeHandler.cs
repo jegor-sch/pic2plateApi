f5393e75173839ae81ffcb7b0d0f8e251bb38904
JegorSch <jegor.schweizer@tobit.com>
2024-09-05 09:56:02 +0200

    💩 Fix this shit

--- pic2plateApi/Handler/RecipeHandler.cs
+++ pic2plateApi/Handler/RecipeHandler.cs
@@ -1,6 +1,21 @@
-﻿namespace pic2plateApi.Handler;
+﻿using pic2plateApi.Model;
+using pic2plateApi.Repository;
+
+namespace pic2plateApi.Handler;
 
 public class RecipeHandler
 {
-    
+    private readonly RecipeRepository _recipeRepo;
+
+    public RecipeHandler(RecipeRepository recipeRepo)
+    {
+        _recipeRepo = recipeRepo;
+    }
+
+    public async Task<int> SaveRecipe(RecipeDto recipe)
+    {
+        var createdRecipeId = await _recipeRepo.SaveRecipe(recipe);
+
+        return createdRecipeId;
+    }
 }
