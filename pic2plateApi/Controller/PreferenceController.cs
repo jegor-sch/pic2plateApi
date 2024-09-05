0dae3e94eb60d8fcf6be64f7ac959abc411fd0cd
JegorSch <jegor.schweizer@tobit.com>
2024-09-05 09:08:42 +0200

    ✨ Add logic to Controller,Handler,Repo

--- pic2plateApi/Controller/PreferenceController.cs
+++ pic2plateApi/Controller/PreferenceController.cs
@@ -1,6 +1,20 @@
-﻿namespace pic2plateApi.Controller;
+﻿using Microsoft.AspNetCore.Mvc;
+using pic2plateApi.Handler;
+
+namespace pic2plateApi.Controller;
 
 public class PreferenceController : Microsoft.AspNetCore.Mvc.Controller
 {
-    
+    private readonly PreferenceHandler _preferenceHandler;
+
+    public PreferenceController(PreferenceHandler preferenceHandler)
+    {
+        _preferenceHandler = preferenceHandler;
+    }
+
+    [HttpGet()]
+    public async Task<IActionResult> Get()
+    {
+        return await _preferenceHandler.Get();
+    }
 }
