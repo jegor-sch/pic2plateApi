34b8af3d545cee96fcf9a438c94f149b2f7afe9a
Gizem Türkmen <gizem.tuerkmen@tobit.com>
2024-09-05 08:54:50 +0200

    ✨ Add recipe controller

--- pic2plateApi/Controller/RecipeController.cs
+++ pic2plateApi/Controller/RecipeController.cs
@@ -1,7 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using pic2plateApi.Handler;
 
 namespace pic2plateApi.Controller;
 
 public class RecipeController : Microsoft.AspNetCore.Mvc.Controller
 {
+    private readonly RecipeHandler _recipeHandler;
+
+    public RecipeController(RecipeHandler recipeHandler)
+    {
+        _recipeHandler = recipeHandler;
+    }
+
+    [HttpPost("")]
+    [ProducesResponseType(typeof(int), (int)HttpStatusCode.NoContent)]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+    public async Task<IActionResult> Post([FromBody] int id)
+    {
+        return NoContent();
+    }
 }
